0ae820041a90672e62295a53dd8ee70bfc454638
Kota Kato <peony.btn@gmail.com>
2021-12-15 16:33:28 +0900

    字句解析器

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kat0h/monkey
+
+go 1.16
---
+++ token/token.go
@@ -0,0 +1,65 @@
+package token
+
+type TokenType string
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// 演算子 + リテラル
+	IDENT = "IDENT" // add, foobar, x, y
+	INT   = "INT"
+
+	// 演算子
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT     = "<"
+	GT     = ">"
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// デリミタ
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// キーワード
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	RETURN   = "RETURN"
+	ELSE     = "ELSE"
+	IF       = "IF"
+	FALSE    = "FALSE"
+)
